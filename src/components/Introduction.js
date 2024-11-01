7b82875cd1df4258d98743386a015351f322c092
musicplatforms <trackpump.com@gmail.com>
2024-11-01 13:12:46 +0100

    Started working on sidebar: edited introduction, currently stuck at StakingOverview

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Box, Typography, Button, Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+import rehypeKatex from 'rehype-katex';
+import 'katex/dist/katex.min.css';
 import sidebarContent from '../sidebarContent_new.json';
 
 function Introduction() {
@@ -12,11 +15,9 @@ function Introduction() {
         <Typography variant="h4" gutterBottom>
           {content.title}
         </Typography>
-        {content.content.map((paragraph, index) => (
-          <Typography key={index} paragraph>
-            {paragraph}
-          </Typography>
-        ))}
+        <ReactMarkdown rehypePlugins={[rehypeKatex]}>
+          {content.content.join('\n\n')}
+        </ReactMarkdown>
         <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
           <Button component={Link} to="/staking-overview" variant="contained" color="primary">
             Start
